a4444bbb29c31c0dce68f51e778f9c697fcf650c
sagar jadhav <sagar.jadhav@novat-tekno.com>
2023-11-11 01:33:25 +0530

    with reducer state and handle possible errors

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,31 +1,73 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import ColorCount from "../Components/ColorCount";
+import { Alert } from "react-native";
 
 const COLOR_INCREAMENT = 15;
 const COLOR_DECREAMENT = 15;
-
+const MAX_COLOR = 255;
+const MIN_COLOR = 0;
 const SquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
-  console.log(red);
+  const reducer = (state, action) => {
+    switch (action.colorToChange) {
+      case "red":
+        return {
+          ...state,
+          red: validateColor(state.red + action.amount),
+        };
+      case "green":
+        return {
+          ...state,
+          green: validateColor(state.green + action.amount),
+        };
+      case "blue":
+        return {
+          ...state,
+          blue: validateColor(state.blue + action.amount),
+        };
+      default:
+        return state;
+    }
+  };
+
+  const validateColor = (color) => {
+    if (color < MIN_COLOR || color > MAX_COLOR) {
+      Alert.alert("Invalid Color", "this kind of color is not available");
+      return null;
+    }
+    return color;
+  };
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+  console.log("state", state);
+  const { red, green, blue } = state;
   return (
     <View>
       <ColorCount
         color={"Red"}
-        onIncrease={() => setRed(Math.min(255, red + COLOR_INCREAMENT))}
-        onDecrease={() => setRed(Math.max(0, red - COLOR_DECREAMENT))}
+        onIncrease={() =>
+          dispatch({ colorToChange: "red", amount: COLOR_INCREAMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "red", amount: -COLOR_DECREAMENT })
+        }
       />
       <ColorCount
         color={"Blue"}
-        onIncrease={() => setBlue(Math.min(255, blue + COLOR_INCREAMENT))}
-        onDecrease={() => setBlue(Math.max(0, blue - COLOR_DECREAMENT))}
+        onIncrease={() =>
+          dispatch({ colorToChange: "green", amount: COLOR_INCREAMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "green", amount: -COLOR_DECREAMENT })
+        }
       />
       <ColorCount
         color={"Green"}
-        onIncrease={() => setGreen(Math.min(255, green + COLOR_INCREAMENT))}
-        onDecrease={() => setGreen(Math.max(0, green - COLOR_DECREAMENT))}
+        onIncrease={() =>
+          dispatch({ colorToChange: "blue", amount: COLOR_INCREAMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "blue", amount: -COLOR_DECREAMENT })
+        }
       />
       <View
         style={{
