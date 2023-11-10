ef55807cf48b18aed1aa35bfc4ee360dc5d2f882
sagar jadhav <sagar.jadhav@novat-tekno.com>
2023-11-11 01:50:21 +0530

    handled errors

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,7 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React, { useReducer } from "react";
 import ColorCount from "../Components/ColorCount";
-import { Alert } from "react-native";
 
 const COLOR_INCREAMENT = 15;
 const COLOR_DECREAMENT = 15;
@@ -31,12 +30,15 @@ const SquareScreen = () => {
   };
 
   const validateColor = (color) => {
-    if (color < MIN_COLOR || color > MAX_COLOR) {
-      Alert.alert("Invalid Color", "this kind of color is not available");
-      return null;
+    if (color < MIN_COLOR) {
+      return MAX_COLOR;
+    }
+    if (color > MAX_COLOR) {
+      return MIN_COLOR;
     }
     return color;
   };
+
   const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
   console.log("state", state);
   const { red, green, blue } = state;
